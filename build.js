06f95f9e10ca24e04f3dda752e861fe32ee9684d
Tobias Koppers <tobias.koppers@googlemail.com>
2012-07-16 07:18:31 +0200

    missing dep, pretty output, fixes #1

--- build.js
+++ build.js
@@ -7,8 +7,8 @@ console.log("compiling javascript...");
 webpack(__dirname, "./lib/client.js", require("./build.json"), function(err, result) {
 	if(err) throw err;
 	var hash = result.hash;
-	var raw = webpackFormatOutput(result, {colors: false});
-	var pretty = webpackFormatOutput(result, {colors: true});
+	var raw = webpackFormatOutput(result, {colors: false, context: __dirname});
+	var pretty = webpackFormatOutput(result, {colors: true, context: __dirname});
 	console.log(pretty);
 	console.log("compiling index.jade...");
 	require("fs").writeFile("index.html", require("./index.jade")({output: raw, hash: hash}), "utf-8", function(err) {
